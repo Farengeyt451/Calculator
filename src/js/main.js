3952b8dbcac173dd2aadac6d4d8f1022e710671c
Alexander <strannik11.92@gmail.com>
2017-11-30 14:31:57 +0300

    Add basic calc functionality

--- src/js/main.js
+++ src/js/main.js
@@ -1,54 +1,91 @@
 const numberBtns = document.querySelectorAll(".number");
 const operationBtns = document.querySelectorAll(".operation");
 const decimalBtn = document.querySelector("#decimal");
-const resultBtn = document.querySelector("#result");
-const clearBtn = document.querySelector("#ce");
+const clearBtn = document.querySelector("#clear");
 const outputRes = document.querySelector("#output");
+const onOffBtn = document.querySelector("#onOff");
+
 let memCurNum = 0;
 let memNewNum = false;
 let memPending = "";
 
-// numberBtns.forEach((btn, index) => {
-// 	let number = numberBtns[index];
-// 	btn.addEventListener("click", function(e) {
-// 		console.log(e);
-// 		pressNumber(e.target.textContent);
-// 	});
-// });
+function onOff() {
+	if (onOffBtn.checked) {
+		outputRes.style.visibility = "visible";
+	} else {
+		outputRes.style.visibility = "hidden";
+		clear();
+	}
+}
+
+function pressNumber(e) {
+	if (!onOffBtn.checked) {
+		outputRes.style.visibility = "visible";
+		onOffBtn.checked = true;
+	}
+	if (memNewNum) {
+		outputRes.value = this.value;
+		memNewNum = false;
+	} else {
+		if (outputRes.value === "0") {
+			outputRes.value = this.value;
+		} else {
+			outputRes.value += this.value;
+		}
+	}
+}
+
+function pressOperation(e) {
+	let memLocalValue = outputRes.value;
+	if (memNewNum && memPending !== "=") {
+		outputRes.value = memCurNum;
+	} else {
+		memNewNum = true;
+		if (memPending === "+") {
+			memCurNum += parseFloat(memLocalValue);
+		} else if (memPending === "-") {
+			memCurNum -= parseFloat(memLocalValue);
+		} else if (memPending === "*") {
+			memCurNum *= parseFloat(memLocalValue);
+		} else if (memPending === "/") {
+			memCurNum /= parseFloat(memLocalValue);
+		} else {
+			memCurNum = parseFloat(memLocalValue);
+		}
+		outputRes.value = memCurNum;
+		memPending = this.value;
+	}
+}
+
+function decimal() {
+	let memDecimal = outputRes.value;
+	if (memNewNum) {
+		memDecimal = "0.";
+		memNewNum = false;
+	} else {
+		if (memDecimal.indexOf(".") === -1) {
+			memDecimal += ".";
+		}
+	}
+	outputRes.value = memDecimal;
+}
+
+function clear() {
+	outputRes.value = "0";
+	memNewNum = true;
+	memCurNum = 0;
+	memPending = "";
+}
+
+onOffBtn.addEventListener("change", onOff);
 
 numberBtns.forEach((btn, index) => {
-	let number = numberBtns[index];
 	btn.addEventListener("click", pressNumber);
 });
 
 operationBtns.forEach((btn, index) => {
-	let operation = operationBtns[index];
 	btn.addEventListener("click", pressOperation);
 });
 
-decimalBtn.addEventListener("click", pressNumber);
-// resultBtn.addEventListener("click", pressNumber);
+decimalBtn.addEventListener("click", decimal);
 clearBtn.addEventListener("click", clear);
-
-function pressNumber(e) {
-	console.log(this.value);
-	if (outputRes.value === "0") {
-		outputRes.value = e.target.textContent;
-	} else {
-		outputRes.value += e.target.textContent;
-	}
-
-	console.log(e.target.textContent);
-}
-
-function pressOperation(e) {
-	console.log(this.value);
-}
-
-function decimal() {
-
-}
-
-function clear() {
-
-}
