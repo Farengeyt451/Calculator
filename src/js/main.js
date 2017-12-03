7185075f001617c0d8a968ad3c285fe7c1c69c1d
Alexander <strannik11.92@gmail.com>
2017-12-03 19:11:21 +0300

    Add trimming value if it length > 14

--- src/js/main.js
+++ src/js/main.js
@@ -8,6 +8,7 @@ const onOffBtn = document.querySelector("#onOff");
 let memCurNum = 0;
 let memNewNum = false;
 let memPending = "";
+let intermediateVal = 0;
 
 function onOff() {
 	if (onOffBtn.checked) {
@@ -18,40 +19,55 @@ function onOff() {
 	}
 }
 
-function checkLength(value) {
-	return outputRes.value.length < 14;
-}
-
-function pressNumber(e) {
+function onIfButtonPressed() {
 	if (!onOffBtn.checked) {
 		outputRes.style.visibility = "visible";
 		onOffBtn.checked = true;
 	}
+}
+
+// Output results to display
+function showRes(num) {
+	let stringNum = num.toString();
+	console.log(stringNum + " " + stringNum.length + " " + typeof stringNum);
+	if (stringNum.length > 14) {
+		if (num > 1) {
+			if (stringNum.indexOf(".") === -1) {
+				outputRes.value = stringNum.slice(0, 1) + "." + stringNum.slice(1, 10) + "e+" + (stringNum.length - 1);
+			} else {
+				outputRes.value = stringNum.slice(0, 1) + stringNum.slice(1, 10) + "e+" + (stringNum.length - 1);
+			}
+		} else {
+			console.log(stringNum.search(/[1-9]/));
+			let startDecimal = stringNum.search(/[1-9]/);
+			outputRes.value = stringNum.slice(startDecimal, startDecimal + 1) + "." + stringNum.slice(startDecimal + 1, 11 + startDecimal) + "e-" + (startDecimal - 1);
+		}
+	} else {
+		outputRes.value = stringNum;
+	}
+}
+
+function pressNumber(e) {
+	onIfButtonPressed();
 	if (memNewNum) {
-		outputRes.value = this.value;
+		intermediateVal = this.value;
+		showRes(intermediateVal);
 		memNewNum = false;
 	} else {
 		if (outputRes.value === "0") {
-			outputRes.value = this.value;
+			intermediateVal = this.value;
+			showRes(intermediateVal);
 		} else {
-			if (checkLength(outputRes.value)) outputRes.value += this.value;
+			intermediateVal += this.value;
+			showRes(intermediateVal);
 		}
 	}
 }
 
 function pressOperation(e) {
-	let memLocalValue = outputRes.value;
+	let memLocalValue = intermediateVal;
 	if (memNewNum && memPending !== "=") {
-		// outputRes.value = memCurNum;
-		if (memCurNum.toString().length > 14) {
-			console.info("> 14");
-			outputRes.value = memCurNum.toString().slice(0, 1) + "." + memCurNum.toString().slice(1, 10) + "e+" + (memCurNum.toString().length - 1);
-			console.log(memCurNum);
-			console.log(memCurNum.toString().length);
-		} else {
-			console.info("< 14");
-			outputRes.value = memCurNum;
-		}
+		showRes(intermediateVal);
 	} else {
 		memNewNum = true;
 		if (memPending === "+") {
@@ -65,34 +81,31 @@ function pressOperation(e) {
 		} else {
 			memCurNum = parseFloat(memLocalValue);
 		}
-		if (memCurNum.toString().length > 14) {
-			console.info("> 14");
-			outputRes.value = memCurNum.toString().slice(0, 1) + "." + memCurNum.toString().slice(1, 10) + "e+" + (memCurNum.toString().length - 1);
-			console.log(memCurNum);
-			console.log(memCurNum.toString().length);
-		} else {
-			console.info("< 14");
-			outputRes.value = memCurNum;
-		}
+		intermediateVal = memCurNum;
 		memPending = this.value;
+		showRes(intermediateVal);
 	}
 }
 
 function decimal() {
-	let memDecimal = outputRes.value;
+	let memDecimal = intermediateVal;
+	onIfButtonPressed();
+	console.log(memDecimal + " memDec");
 	if (memNewNum) {
 		memDecimal = "0.";
 		memNewNum = false;
 	} else {
-		if (memDecimal.indexOf(".") === -1) {
+		if (memDecimal.toString().indexOf(".") === -1) {
 			memDecimal += ".";
 		}
 	}
-	outputRes.value = memDecimal;
+	intermediateVal = memDecimal;
+	showRes(memDecimal);
 }
 
 function clear() {
 	outputRes.value = "0";
+	intermediateVal = "0";
 	memNewNum = true;
 	memCurNum = 0;
 	memPending = "";
